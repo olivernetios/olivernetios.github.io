1fefecce9d1573d28f09a1d0310d01d295c9aa30
olivernetios <139788912+olivernetios@users.noreply.github.com>
2023-07-22 14:25:38 -0300

    l

--- estudos html e css/base1/base.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector("button")
-const modal = document.querySelector("dialog")
-const buttonclose = document.querySelector("dialog button")
-
-button.onclick = function() {
-    modal.show()
-}
